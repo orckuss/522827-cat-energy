8219edbba82976fedde16173a34545a027967fec
Дмитрий Васильев <vasilev8dmitriy@gmail.com>
2019-05-10 16:38:58 +0300

    Инициализация проекта

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,47 @@
 "use strict";
 
 var gulp = require("gulp");
+var del = require("del");
+var posthtml = require("gulp-posthtml");
+var include = require("posthtml-include");
+var htmlmin = require("gulp-htmlmin");
 var sass = require("gulp-sass");
+var csso = require("gulp-csso");
+var uglify = require("gulp-uglify");
 var plumber = require("gulp-plumber");
+var rename = require("gulp-rename");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
 var server = require("browser-sync").create();
+var imagemin = require("gulp-imagemin");
+var webp = require("gulp-webp");
+var svgstore = require("gulp-svgstore");
+
+gulp.task("copy", function () {
+  return gulp.src([
+      "source/fonts/**/*.{woff,woff2}",
+      "source/js/**/*.js"
+    ], {
+      base: "source"
+    })
+    .pipe(gulp.dest("build"));
+});
+
+gulp.task("clean", function () {
+  return del("build");
+});
+
+gulp.task("html", function () {
+  return gulp.src("source/*.html")
+    .pipe(plumber())
+    .pipe(posthtml([
+      include()
+    ]))
+    .pipe(htmlmin({
+      collapseWhitespace: true
+    }))
+    .pipe(gulp.dest("build"));
+});
 
 gulp.task("css", function () {
   return gulp.src("source/sass/style.scss")
@@ -14,21 +50,85 @@ gulp.task("css", function () {
     .pipe(postcss([
       autoprefixer()
     ]))
-    .pipe(gulp.dest("source/css"))
+    .pipe(gulp.dest("build/css"))
+    .pipe(csso())
+    .pipe(rename("style.min.css"))
+    .pipe(gulp.dest("build/css"))
     .pipe(server.stream());
+});
+
+gulp.task("js", function () {
+  return gulp.src("source/js/app.js")
+    .pipe(plumber())
+    .pipe(uglify())
+    .pipe(rename("app.min.js"))
+    .pipe(gulp.dest("build/js"));
+});
+
+gulp.task("imagesOpti", function () {
+  return gulp.src("source/img/*.{jpg,png,svg}")
+    .pipe(plumber())
+    .pipe(imagemin([
+      imagemin.optipng({
+        optimizationLevel: 3
+      }),
+      imagemin.jpegtran({
+        progressive: true
+      }),
+      imagemin.svgo()
+    ]))
+    .pipe(gulp.dest("build/img"));
+});
+
+gulp.task("webp", function () {
+  return gulp.src("source/img/*.{jpg,png}")
+    .pipe(plumber())
+    .pipe(webp({
+      quality: 90
+    }))
+    .pipe(gulp.dest("build/img"));
+});
+
+gulp.task("sprite", function() {
+  return gulp.src("source/img/icons/icon-*.svg")
+    .pipe(plumber())
+    .pipe(svgstore({
+      inlineSvg: true
+    }))
+    .pipe(rename("sprite.svg"))
+    .pipe(gulp.dest("build/img"));
 });
 
 gulp.task("server", function () {
   server.init({
-    server: "source/",
+    server: "build/",
     notify: false,
     open: true,
     cors: true,
     ui: false
   });
 
+  gulp.watch("source/*.html", gulp.series("html", "reload"));
   gulp.watch("source/sass/**/*.{scss,sass}", gulp.series("css"));
-  gulp.watch("source/*.html").on("change", server.reload);
+  gulp.watch("source/js/*.js", gulp.series("js", "reload"));
+  gulp.watch("source/img/*", gulp.series("imagesOpti", "webp"));
+  gulp.watch("source/img/icons/*", gulp.series("sprite", "html", "reload"));
 });
 
-gulp.task("start", gulp.series("css", "server"));
+gulp.task("reload", function(done) {
+  server.reload();
+  done();
+});
+
+gulp.task("build", gulp.series(
+  "clean",
+  "copy",
+  "imagesOpti",
+  "webp",
+  "sprite",
+  "html",
+  "css",
+  "js"
+));
+
+gulp.task("start", gulp.series("build", "server"));
