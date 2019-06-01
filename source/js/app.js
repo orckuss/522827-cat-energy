21b28d63edd2b71435d7c1551128af1e95030228
Дмитрий Васильев <vasilev8dmitriy@gmail.com>
2019-06-01 13:17:42 +0300

    Изменены скрипты переключения изображения в блоке образца под планшет и десктоп

--- source/js/app.js
+++ source/js/app.js
@@ -6,14 +6,16 @@ $('.js-nav-btn').click(function() {
 
 $('.js-btn-before').click(function() {
   $('.toggle-button').addClass('toggle-button--off').removeClass('toggle-button--on');
-  $('.js-img-before').css('width', 'auto');
+  $('.js-img-before').css('width', '100%');
   $('.js-img-after').css('width', '0');
+  $('.js-btn-scale').css('left', '0');
 });
 
 $('.js-btn-after').click(function() {
   $('.toggle-button').addClass('toggle-button--on').removeClass('toggle-button--off');
-  $('.js-img-after').css('width', 'auto');
+  $('.js-img-after').css('width', '100%');
   $('.js-img-before').css('width', '0');
+  $('.js-btn-scale').css('left', '100%');
 });
 
 $('.toggle-button').click(function() {
