447a592173022b59f69c56894f03d29bfe5403d7
Дмитрий Васильев <vasilev8dmitriy@gmail.com>
2019-06-01 12:02:29 +0300

    Реализованы скрипты переключения изображений в мобильной версии блока с образцом

--- source/js/app.js
+++ source/js/app.js
@@ -3,3 +3,26 @@ $('.js-nav-btn').click(function() {
   $(this).toggleClass('nav__btn--opened');
   $('.js-nav-menu').slideToggle('fast');
 });
+
+$('.js-btn-before').click(function() {
+  $('.toggle-button').addClass('toggle-button--off').removeClass('toggle-button--on');
+  $('.js-img-before').css('width', 'auto');
+  $('.js-img-after').css('width', '0');
+});
+
+$('.js-btn-after').click(function() {
+  $('.toggle-button').addClass('toggle-button--on').removeClass('toggle-button--off');
+  $('.js-img-after').css('width', 'auto');
+  $('.js-img-before').css('width', '0');
+});
+
+$('.toggle-button').click(function() {
+  $(this).toggleClass('toggle-button--off toggle-button--on');
+  if ($('.js-img-before').width() === 0) {
+    $('.js-img-before').css('width', 'auto');
+    $('.js-img-after').css('width', '0');
+  } else if ($('.js-img-after').width() === 0) {
+    $('.js-img-after').css('width', 'auto');
+    $('.js-img-before').css('width', '0');
+  }
+});
